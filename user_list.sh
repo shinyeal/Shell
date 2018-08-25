d529c87f6528a1bf57a6cf5b599066422964fe6b
xinyue <1670806387@qq.com>
2018-08-25 21:08:08 +0800

    update the file

--- user_list.sh
+++ user_list.sh
@@ -5,7 +5,7 @@
 
 time_now=`date +"%Y-%m-%d__%H:%M:%S"`
 user_count=`cat /etc/passwd | grep "[a-zA-Z0-9]" | grep -v "wtmp" | grep bash | grep -v "boot" | wc -l`
-always_user=`last | cut -d " " -f 1 | tr -s " " "\n" | grep "[a-z]" | sort | uniq -c | sort -n -r | head -n 3 | awk '{printf("%s\n", $2)}' | xargs | tr " " ","`
+always_user=`last | cut -d " " -f 1 | tr -s " " "\n" | grep "[a-z]" | grep -v "wtmp" | sort | uniq -c | sort -n -r | head -n 3 | awk '{printf("%s\n", $2)}' | xargs | tr " " ","`
 root=`cat /etc/group | sort | grep sudo | cut -d : -f 4`
 tty=`w -h | awk '{printf("%s_%s_%s\n", $1,$3,$2)}' | xargs | tr " " ","`
 
