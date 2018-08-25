b9a9008012ddc53c63d8d330ed1bc97dc16005c7
xinyue <1670806387@qq.com>
2018-08-25 21:07:24 +0800

    update the file

--- shell12.sh
+++ shell12.sh
@@ -5,11 +5,14 @@
 
 time_now=`date +"%Y-%m-%d__%H:%M:%S"`
 last=$1
+if  [ ! $last  ];then
+    echo "error"
+else
+    total=`free -m | head -n 2 | tail -n 1 | awk '{printf("%s",$2)}'`
+    free=`free -m | head -n 2 | tail -n 1 | awk '{printf("%s",$2 - $3)}'`
+    used=`free -m |head -n 2 | tail -n 1 | awk '{printf("%s",$3)}'`
+    present=`echo "scale=1;${used} * 100 / ${total}" | bc`
+    equal=`echo "scale=1;0.7 * ${present} + 0.3 * ${last}" | bc`
 
-total=`free -m | head -n 2 | tail -n 1 | awk '{printf("%s",$2)}'`
-free=`free -m | head -n 2 | tail -n 1 | awk '{printf("%s",$2 - $3)}'`
-used=`free -m |head -n 2 | tail -n 1 | awk '{printf("%s",$3)}'`
-present=`echo "scale=1;${used} * 100 / ${total}" | bc`
-equal=`echo "scale=1;0.7 * ${present} + 0.3 * ${last}" | bc`
-
-echo ${time_now} ${total} ${free} ${present}% ${equal}%
+    echo ${time_now} ${total} ${free} ${present}% ${equal}%
+fi
