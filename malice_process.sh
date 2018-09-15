63110da1ea3ac420111faf25c63a7d3b3658634c
xinyue <1670806387@qq.com>
2018-09-15 19:52:14 +0800

    update the file

--- malice_process.sh
+++ malice_process.sh
@@ -3,11 +3,19 @@
 # 恶意进程检测
 
 time_now=`date +"%Y-%m-%d__%H:%M:%S"`
-pid=( `ps aux | awk '{if($3 > 50 || $4 > 50) {printf("%s", $2)}}'` )
+pid=( `ps aux | awk '{if($3 > 50 || $4 > 50) {print $2}}'` )
+
+#echo ${#pid[@]}
+#for (( o=0;o<${#pid[@]};o++ ));do
+#    echo ${pid[o]}
+#done
+
 if [[ $pid ]];then
     sleep 5
-    process=`ps aux | awk '{if($3 > 50 || $4 > 50) {printf("%s %s %s %.2f%% %.2f%%\n", $11, $2, $1, $3, $4)}}'` 
-    echo $time_now $process
+    for ((i = 0; i < ${#pid[@]}; i += 1 ));do
+        process=( `ps aux | awk '{if($3 > 50 || $4 > 50) {printf("%s %s %s %.2f%% %.2f%%\n", $11, $2, $1, $3, $4)}}'` ) 
+        echo $time_now ${process[0 + i * 5]} ${process[1 + i * 5]} ${process[2 + i * 5]} ${process[3 + i * 5]} ${process[4 + i * 5]}
+    done
 else
-    echo "error"
+    echo "没有发现恶意进程"
 fi
